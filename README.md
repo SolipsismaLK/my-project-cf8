caf259c675f0277c2eedde43b3a6c4c3d4e547ad
konstantinoslisgaras <konstantinoslisgaras@gmail.com>
2025-05-14 19:07:25 +0300

    feat: add project again

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# my-project-cf8
+# **my-project-cf8**
+### **Requirements**
+
+---
+
+- JDK17
+
+## **Usage**
+
+---
+
+1. Just run the HelloWorld class of this app.
---
+++ src/gr/aueb/cf/helloApp/HelloWorld.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.helloApp;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        System.out.print("Konstantinos' First Project!");
+        System.out.print("I am so eager!");
+    }
+}
