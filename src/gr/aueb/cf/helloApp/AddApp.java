a8ca5123652c9da6c6ef1dccc66400d18a275ffb
konstantinoslisgaras <konstantinoslisgaras@gmail.com>
2025-05-14 19:40:25 +0300

    feat: add addition class

---
+++ src/gr/aueb/cf/helloApp/AddApp.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.helloApp;
+
+public class AddApp {
+
+    public static void main(String[] args) {
+        System.out.println(add(3, 5));
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
